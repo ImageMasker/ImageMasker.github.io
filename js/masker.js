8aa18ad93e3e6ba975903038697d6c94aaf1867e
ScrantonicityII <superfreakonomics123@gmail.com>
2018-02-19 22:06:23 -0800

    added a fake canvas for preview, error alert on upload failure

--- js/masker.js
+++ js/masker.js
@@ -1,1 +1,183 @@
-var height,width,baseImage=null,maskImage=null,finalImage=null,mask=null,canvas=new fabric.Canvas("canvas");function addProxyToUrl(e){return url="https://cors-anywhere.herokuapp.com/"+e.replace(/(^\w+:|^)\/\//,"")}function checkURL(e){null!=e.match(/\.(jpeg|jpg|gif|png)$/)&&(loadSourceImage())}function loadSourceImage(){baseUrl=document.getElementById("original").value,""==baseUrl?sourceImageUrl="images/placeholder.png":sourceImageUrl=addProxyToUrl(document.getElementById("original").value),fabric.util.loadImage(sourceImageUrl,function(e){baseImage=e,height=e.height,width=e.width,canvas.setHeight(height).setWidth(width),canvas.setBackgroundImage(new fabric.Image(e),canvas.renderAll.bind(canvas),{width:width,height:height,originX:0,originY:0})},null,"Anonymous")}function uploadMaskImage(e){var a=e.target.files[0].type,t=URL.createObjectURL(e.target.files[0]);alpha=document.getElementById("alpha").value/100,"image/png"===a&&(finalImage&&canvas.remove(finalImage),fabric.Image.fromURL(t,function(e){e.set({cornerSize:15,borderColor:"white",cornerColor:"white",transparentCorners:!1,hasRotatingPoint:!1,opacity:alpha}),finalImage=e,canvas.add(finalImage)}),document.getElementById("uploadbutton").style.display="inline-block",document.getElementById("uploadbutton").value="Upload to Imgur",document.getElementById("uploadbutton").disabled=!1,document.getElementById("uploadedUrl").style.display="none",document.getElementById("copyToClipboard").style.display="none",document.getElementById("checkForRIS").style.display="none")}function loadMaskImage(e){alpha=document.getElementById("alpha").value/100,maskImageUrl=e.src,fabric.util.loadImage(maskImageUrl,function(e){maskImage=e,finalImage&&canvas.remove(finalImage),(finalImage=new fabric.Image(maskImage).set({cornerSize:15,borderColor:"white",cornerColor:"white",transparentCorners:!1,hasRotatingPoint:!1})).set("opacity",alpha),canvas.add(finalImage)},null,"Anonymous"),document.getElementById("uploadbutton").style.display="inline-block",document.getElementById("uploadbutton").value="Upload to Imgur",document.getElementById("uploadbutton").disabled=!1,document.getElementById("uploadedUrl").style.display="none",document.getElementById("copyToClipboard").style.display="none",document.getElementById("checkForRIS").style.display="none"}function upload(){var e=document.getElementById("canvas").toDataURL("image/png").split(",")[1];$.ajax({url:"https://api.imgur.com/3/image",type:"post",headers:{Authorization:"Client-ID 9c586fafe6ec100"},data:{image:e},dataType:"json",success:function(e){e.success?(document.getElementById("uploadedUrl").value=e.data.link,document.getElementById("uploadbutton").style.display="none",document.getElementById("uploadedUrl").style.display="inline-block",document.getElementById("copyToClipboard").style.display="inline-block",document.getElementById("checkForRIS").style.display="inline-block"):document.getElementById("uploadedUrl").value="Failed to upload"}}),document.getElementById("uploadbutton").value="Uploading...",document.getElementById("uploadbutton").disabled=!0}function copyUrl(){document.getElementById("uploadedUrl").select(),document.execCommand("Copy")}function checkRIS(){var e=document.getElementById("uploadedUrl").value;window.open("http://www.google.com/searchbyimage?image_url="+e),window.open("https://www.bing.com/images/searchbyimage?cbir=ssbi&pageurl=http%3A%2F%2Fwww.squobble.com&imgurl="+e),window.open("https://www.yandex.com/images/search?rpt=imageview&img_url="+e),window.open("http://www.tineye.com/search/?url="+e)}loadSourceImage();
+var baseImage = null;
+var maskImage = null;
+var fakeMaskImage = null;
+var height, width, newHeight, newWidth;
+var mask = null;
+var realCanvas = new fabric.Canvas("realCanvas");
+var fakeCanvas = new fabric.Canvas("fakeCanvas");
+
+function addProxyToUrl(baseUrl) {
+  return url = "https://cors-anywhere.herokuapp.com/" + baseUrl.replace(/(^\w+:|^)\/\//, '');
+}
+
+function checkURL(url) {
+  if(url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+    loadSourceImage();
+  }
+}
+
+function calculateDimensions(h, w) {
+  newHeight = h;
+  newWidth = w;
+  if (w > h) {
+    if (w > 1000) {
+      newHeight *= 1000/w;
+      newWidth = 1000;
+    }
+  } else {
+    if (h > 1000) {
+      newWidth *= 1000/h;
+      newHeight = 1000;
+    }
+  }
+}
+
+function requiresResize(ih, iw, mh, mw) {
+  if (ih > mh || iw > mw)
+    return true;
+  else
+    return false;
+}
+
+function loadSourceImage() {
+    baseUrl = document.getElementById('original').value;
+    if (baseUrl == "") {
+        sourceImageUrl = "images/placeholder.png"
+    } else {
+        sourceImageUrl = addProxyToUrl(document.getElementById('original').value);
+    }
+    fabric.util.loadImage(sourceImageUrl, function(_baseImage) {
+        baseImage = _baseImage;
+        height = _baseImage.height;
+        width = _baseImage.width;
+        calculateDimensions(_baseImage.height, _baseImage.width);
+        console.log(newHeight, newWidth);
+        fakeCanvas.setHeight(newHeight).setWidth(newWidth);
+        realCanvas.setHeight(height).setWidth(width);
+        realCanvas.setBackgroundImage(new fabric.Image(_baseImage), realCanvas.renderAll.bind(realCanvas), {
+            width: width,
+            height: height,
+            originX: 0,
+            originY: 0
+        });
+        fakeCanvas.setBackgroundImage(new fabric.Image(_baseImage), fakeCanvas.renderAll.bind(fakeCanvas), {
+          scaleX: fakeCanvas.width / width,
+          scaleY: fakeCanvas.height / height
+        });
+    }, null, "Anonymous");
+}
+loadSourceImage();
+
+function uploadMaskImage(e) {
+  var filetype = e.target.files[0].type;
+  var url = URL.createObjectURL(e.target.files[0]);
+  alpha = document.getElementById('alpha').value / 100;
+
+  if (filetype === "image/png") {
+    if (maskImage) {
+      fakeCanvas.remove(fakeMaskImage);
+      realCanvas.remove(maskImage);
+    }
+    fabric.Image.fromURL(url, function(img) {
+      maskImage = img;
+      fakeMaskImage = img;
+      maskImage.set({
+        scaleX: realCanvas.width / img.width,
+        scaleY: realCanvas.height / img.height,
+        opacity: alpha
+      });
+      fakeMaskImage.set({
+        scaleX: realCanvas.width / maskImage.width,
+        scaleY: realCanvas.height / maskImage.height,
+        opacity: alpha
+      });
+      
+      fakeMaskImage = img;
+      realCanvas.add(maskImage);
+      fakeCanvas.add(fakeMaskImage);
+    });
+    document.getElementById('uploadbutton').style.display = "inline-block";
+    document.getElementById('uploadbutton').value = "Upload to Imgur";
+    document.getElementById('uploadbutton').disabled = false;
+    document.getElementById('uploadedUrl').style.display = "none";
+    document.getElementById('copyToClipboard').style.display = "none";
+    document.getElementById('checkForRIS').style.display = "none";
+}
+}
+
+function loadMaskImage(selectedMask) {
+    alpha = document.getElementById('alpha').value / 100;
+    maskImageUrl = selectedMask.src;
+    fabric.util.loadImage(maskImageUrl, function(_maskImage) {
+        if (maskImage) {
+            fakeCanvas.remove(fakeMaskImage);
+            realCanvas.remove(maskImage);
+        }
+        maskImage = new fabric.Image(_maskImage).set({
+          opacity: alpha,
+          scaleX: realCanvas.width / _maskImage.width,
+          scaleY: realCanvas.height / _maskImage.height
+        });
+        fakeMaskImage = new fabric.Image(_maskImage).set({
+          opacity: alpha,
+          scaleX: realCanvas.width / maskImage.width,
+          scaleY: realCanvas.height / maskImage.height,
+        });
+        realCanvas.add(maskImage);
+        fakeCanvas.add(fakeMaskImage);
+    }, null, "Anonymous");
+    document.getElementById('uploadbutton').style.display = "inline-block";
+    document.getElementById('uploadbutton').value = "Upload to Imgur";
+    document.getElementById('uploadbutton').disabled = false;
+    document.getElementById('uploadedUrl').style.display = "none";
+    document.getElementById('copyToClipboard').style.display = "none";
+    document.getElementById('checkForRIS').style.display = "none";
+}
+
+function upload() {
+  var img = document.getElementById('realCanvas').toDataURL('image/png').split(',')[1];
+
+  $.ajax({
+      url: 'https://api.imgur.com/3/image',
+      type: 'post',
+      headers: {
+          Authorization: 'Client-ID 9c586fafe6ec100'
+      },
+      data: {
+          image: img
+      },
+      dataType: 'json',
+      error: function(response) {
+        alert("Error uploading to Imgur. Reason: " + response.responseJSON.data.error);
+        document.getElementById('uploadbutton').value = "Upload to Imgur";
+        document.getElementById('uploadbutton').disabled = false;
+      },
+      success: function(response) {
+          if(response.success) {
+              document.getElementById('uploadedUrl').value = response.data.link;
+              document.getElementById('uploadbutton').style.display = "none";
+              document.getElementById('uploadedUrl').style.display = "inline-block";
+              document.getElementById('copyToClipboard').style.display = "inline-block";
+              document.getElementById('checkForRIS').style.display = "inline-block";
+          } else {
+            alert("Failed to upload.");
+          }
+      }
+  });
+document.getElementById('uploadbutton').value = "Uploading...";
+document.getElementById('uploadbutton').disabled = true;
+}
+
+function copyUrl() {
+  var copyText = document.getElementById("uploadedUrl");
+  copyText.select();
+  document.execCommand("Copy");
+}
+
+function checkRIS() {
+  var url = document.getElementById("uploadedUrl").value;
+  window.open("http://www.google.com/searchbyimage?image_url="+url);
+  window.open("https://www.bing.com/images/searchbyimage?cbir=ssbi&pageurl=http%3A%2F%2Fwww.squobble.com&imgurl="+url);
+  window.open("https://www.yandex.com/images/search?rpt=imageview&img_url="+url);
+  window.open("http://www.tineye.com/search/?url="+url);
+}
