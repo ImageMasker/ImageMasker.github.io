6a7348f8945016bb3e18c0871f920c67740d0251
ScrantonicityII <superfreakonomics123@gmail.com>
2018-02-20 00:00:34 -0800

    one common function to mask, masking with scaling seems to work

--- js/masker.js
+++ js/masker.js
@@ -11,7 +11,7 @@ function addProxyToUrl(baseUrl) {
 }
 
 function checkURL(url) {
-  if(url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+  if(url.match(/\.(jpeg|jpg|png)/) != null) {
     loadSourceImage();
   }
 }
@@ -32,8 +32,8 @@ function calculateDimensions(h, w) {
   }
 }
 
-function requiresResize(ih, iw, mh, mw) {
-  if (ih > mh || iw > mw)
+function requiresResize(id, md) {
+  if (id > md)
     return true;
   else
     return false;
@@ -47,122 +47,95 @@ function loadSourceImage() {
         sourceImageUrl = addProxyToUrl(document.getElementById('original').value);
     }
     fabric.util.loadImage(sourceImageUrl, function(_baseImage) {
-        baseImage = _baseImage;
-        height = _baseImage.height;
-        width = _baseImage.width;
-        calculateDimensions(_baseImage.height, _baseImage.width);
-        console.log(newHeight, newWidth);
-        fakeCanvas.setHeight(newHeight).setWidth(newWidth);
-        realCanvas.setHeight(height).setWidth(width);
-        realCanvas.setBackgroundImage(new fabric.Image(_baseImage), realCanvas.renderAll.bind(realCanvas), {
-            width: width,
-            height: height,
-            originX: 0,
-            originY: 0
-        });
-        fakeCanvas.setBackgroundImage(new fabric.Image(_baseImage), fakeCanvas.renderAll.bind(fakeCanvas), {
-          scaleX: fakeCanvas.width / width,
-          scaleY: fakeCanvas.height / height
-        });
+      if (_baseImage == null) {
+        alert("Something went wrong while loading the image.");
+      }
+      baseImage = _baseImage;
+      height = _baseImage.height;
+      width = _baseImage.width;
+      calculateDimensions(_baseImage.height, _baseImage.width);
+      fakeCanvas.setHeight(newHeight).setWidth(newWidth);
+      realCanvas.setHeight(height).setWidth(width);
+      realCanvas.setBackgroundImage(new fabric.Image(_baseImage), realCanvas.renderAll.bind(realCanvas), {
+          width: width,
+          height: height
+      });
+      fakeCanvas.setBackgroundImage(new fabric.Image(_baseImage), fakeCanvas.renderAll.bind(fakeCanvas), {
+        scaleX: fakeCanvas.width / width,
+        scaleY: fakeCanvas.height / height
+      });
     }, null, "Anonymous");
 }
 loadSourceImage();
 
-function uploadMaskImage(e) {
-  var filetype = e.target.files[0].type;
-  var url = URL.createObjectURL(e.target.files[0]);
-  alpha = document.getElementById('alpha').value / 100;
-
-  if (filetype === "image/png") {
-    if (maskImage) {
-      fakeCanvas.remove(fakeMaskImage);
-      realCanvas.remove(maskImage);
+function loadMask(selectedMask) {
+  var url = "";
+  if ('target' in selectedMask) {
+    var filetype = selectedMask.target.files[0].type;
+    url = URL.createObjectURL(selectedMask.target.files[0]);
+    if (filetype != "image/png") {
+      return;
     }
-    fabric.Image.fromURL(url, function(img) {
-      maskImage = img;
-      fakeMaskImage = img;
-      maskImage.set({
-        scaleX: realCanvas.width / img.width,
-        scaleY: realCanvas.height / img.height,
-        opacity: alpha
-      });
-      fakeMaskImage.set({
-        scaleX: realCanvas.width / maskImage.width,
-        scaleY: realCanvas.height / maskImage.height,
-        opacity: alpha
-      });
-      
-      fakeMaskImage = img;
-      realCanvas.add(maskImage);
-      fakeCanvas.add(fakeMaskImage);
-    });
-    document.getElementById('uploadbutton').style.display = "inline-block";
-    document.getElementById('uploadbutton').value = "Upload to Imgur";
-    document.getElementById('uploadbutton').disabled = false;
-    document.getElementById('uploadedUrl').style.display = "none";
-    document.getElementById('copyToClipboard').style.display = "none";
-    document.getElementById('checkForRIS').style.display = "none";
-}
-}
-
-function loadMaskImage(selectedMask) {
-    alpha = document.getElementById('alpha').value / 100;
-    maskImageUrl = selectedMask.src;
-    fabric.util.loadImage(maskImageUrl, function(_maskImage) {
-        if (maskImage) {
-            fakeCanvas.remove(fakeMaskImage);
-            realCanvas.remove(maskImage);
-        }
-        maskImage = new fabric.Image(_maskImage).set({
-          opacity: alpha,
-          scaleX: realCanvas.width / _maskImage.width,
-          scaleY: realCanvas.height / _maskImage.height
-        });
-        fakeMaskImage = new fabric.Image(_maskImage).set({
-          opacity: alpha,
-          scaleX: realCanvas.width / maskImage.width,
-          scaleY: realCanvas.height / maskImage.height,
-        });
-        realCanvas.add(maskImage);
-        fakeCanvas.add(fakeMaskImage);
-    }, null, "Anonymous");
-    document.getElementById('uploadbutton').style.display = "inline-block";
-    document.getElementById('uploadbutton').value = "Upload to Imgur";
-    document.getElementById('uploadbutton').disabled = false;
-    document.getElementById('uploadedUrl').style.display = "none";
-    document.getElementById('copyToClipboard').style.display = "none";
-    document.getElementById('checkForRIS').style.display = "none";
+  } else {
+    url = selectedMask.src;
+  }
+  alpha = document.getElementById('alpha').value / 100;
+  if (maskImage) {
+    fakeCanvas.remove(fakeMaskImage);
+    realCanvas.remove(maskImage);
+  }
+  fabric.Image.fromURL(url, function(img) {
+    img.set('opacity', alpha);
+    maskImage = img;
+    fakeMaskImage = fabric.util.object.clone(img);
+    if (requiresResize(width, img.width)) {
+      maskImage.set('scaleX', realCanvas.width / img.width);
+      fakeMaskImage.set('scaleX', fakeCanvas.width / maskImage.width);
+    }
+    if(requiresResize(height, img.height)) {
+      maskImage.set('scaleY', realCanvas.height / img.height);
+      fakeMaskImage.set('scaleY', fakeCanvas.height / maskImage.height);
+    }
+    realCanvas.add(maskImage);
+    fakeCanvas.add(fakeMaskImage);
+  });
+  document.getElementById('uploadbutton').style.display = "inline-block";
+  document.getElementById('uploadbutton').value = "Upload to Imgur";
+  document.getElementById('uploadbutton').disabled = false;
+  document.getElementById('uploadedUrl').style.display = "none";
+  document.getElementById('copyToClipboard').style.display = "none";
+  document.getElementById('checkForRIS').style.display = "none";
 }
 
 function upload() {
   var img = document.getElementById('realCanvas').toDataURL('image/png').split(',')[1];
 
   $.ajax({
-      url: 'https://api.imgur.com/3/image',
-      type: 'post',
-      headers: {
-          Authorization: 'Client-ID 9c586fafe6ec100'
-      },
-      data: {
-          image: img
-      },
-      dataType: 'json',
-      error: function(response) {
-        alert("Error uploading to Imgur. Reason: " + response.responseJSON.data.error);
-        document.getElementById('uploadbutton').value = "Upload to Imgur";
-        document.getElementById('uploadbutton').disabled = false;
-      },
-      success: function(response) {
-          if(response.success) {
-              document.getElementById('uploadedUrl').value = response.data.link;
-              document.getElementById('uploadbutton').style.display = "none";
-              document.getElementById('uploadedUrl').style.display = "inline-block";
-              document.getElementById('copyToClipboard').style.display = "inline-block";
-              document.getElementById('checkForRIS').style.display = "inline-block";
-          } else {
-            alert("Failed to upload.");
-          }
+    url: 'https://api.imgur.com/3/image',
+    type: 'post',
+    headers: {
+      Authorization: 'Client-ID 9c586fafe6ec100'
+    },
+    data: {
+      image: img
+    },
+    dataType: 'json',
+    error: function(response) {
+      alert("Error uploading to Imgur. Reason: " + response.responseJSON.data.error);
+      document.getElementById('uploadbutton').value = "Upload to Imgur";
+      document.getElementById('uploadbutton').disabled = false;
+    },
+    success: function(response) {
+      if(response.success) {
+          document.getElementById('uploadedUrl').value = response.data.link;
+          document.getElementById('uploadbutton').style.display = "none";
+          document.getElementById('uploadedUrl').style.display = "inline-block";
+          document.getElementById('copyToClipboard').style.display = "inline-block";
+          document.getElementById('checkForRIS').style.display = "inline-block";
+      } else {
+        alert("Failed to upload.");
       }
+    }
   });
 document.getElementById('uploadbutton').value = "Uploading...";
 document.getElementById('uploadbutton').disabled = true;
