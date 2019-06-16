2897f873bafd83baa68f5100db3c60c02917f14e
cpc2 <cpc2@users.noreply.github.com>
2019-06-16 23:44:07 +0200

    Fix Yandex in mobile RIS input

--- js/masker.js
+++ js/masker.js
@@ -23,7 +23,7 @@ $("html").on("paste", function (event) {
     item.getAsString(function (s) {
       window.open("http://www.tineye.com/search/?url=" + s);
       window.open("http://www.google.com/searchbyimage?image_url=" + s);
-      window.open("https://www.yandex.com/images/search?rpt=imageview&img_url=" + s);
+      window.open("https://yandex.com/images/search?img_url=" + s + "&rpt=imageview");
     });
   }
   else {
